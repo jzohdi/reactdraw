41e60a729c70b250744db102aae7d0f3747b3f6d
jzohdi <31998568+jzohdi@users.noreply.github.com>
2022-07-04 20:40:28 -0400

    working on select tool

--- examples/nextjs-typescript/pages/index.tsx
+++ examples/nextjs-typescript/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { ReactDraw, freeDrawTool } from "../../../dist/index.esm";
+// import { ReactDraw, freeDrawTool } from "../../../dist/index.esm";
 // import { freeDrawTool } from "../../../dist/";
 
 const Home: NextPage = () => {
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <ReactDraw topBarTools={[freeDrawTool]}></ReactDraw>
+        {/* <ReactDraw topBarTools={[freeDrawTool]}></ReactDraw> */}
       </main>
 
       <footer className={styles.footer}>
