833f7f20cdb7ecbf8448a00843dcab9746a93496
jzohdi <31998568+jzohdi@users.noreply.github.com>
2023-11-27 13:12:34 -0500

    added eight point skew

--- src/types.ts
+++ src/types.ts
@@ -296,6 +296,15 @@ export type SelectMode =
   | "resize-ne"
   | "resize-se"
   | "resize-sw";
+
+type Enumerate<N extends number, Acc extends number[] = []> =
+  Acc["length"] extends N ? Acc[number] : Enumerate<N, [...Acc, Acc["length"]]>;
+
+export type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
+
 export type HTMLEvent = keyof HTMLElementEventMap;
 
 export type IntermediateStringableObject = {
--- src/utils/select/utils.ts
+++ src/utils/select/utils.ts
@@ -1,12 +1,28 @@
 import { SELECT_TOOL_ID } from "../../constants";
-import { DrawingData, DrawingTools, ReactDrawContext } from "../../types";
-import { resizeNE, resizeNW, resizeSE, resizeSW } from "../resizeObject";
+import {
+  DrawingData,
+  DrawingTools,
+  IntRange,
+  ReactDrawContext,
+} from "../../types";
+import {
+  resizeE,
+  resizeN,
+  resizeNE,
+  resizeNW,
+  resizeS,
+  resizeSE,
+  resizeSW,
+  resizeW,
+} from "../resizeObject";
 import { getToolById, setStyles } from "../utils";
 import {
   CORNER_BUTTON_PRE,
   ROTATE_BUTTON_PRE,
   ROTATE_BUTTON_STYLES,
   ROTATE_DOTTED_LINE_STYLES,
+  SELECTED_BUTTON_HEIGHT,
+  SELECTED_BUTTON_WIDTH,
   SELECTED_CORNER_BUTTON,
   SELECT_FRAME_DIV_STYLES,
   SELECT_FRAME_PRE,
@@ -100,23 +116,35 @@ function addToolsToSelectionDiv(
   ctx: ReactDrawContext
 ): void {
   const eleId = data.id;
-  // corner buttons
-  const topLeftCorner = cornerButton(eleId, true, false, false, true);
+  // corner/side buttons
+  const topLeftCorner = createButtonWithOffsets(eleId, 2, 0, 0, 2);
   addHandlersToCornerButton(topLeftCorner, eleId, ctx, resizeNW);
-  const topRightCorner = cornerButton(eleId, true, true);
+  const topButton = createButtonWithOffsets(eleId, 2, 1);
+  addHandlersToCornerButton(topButton, eleId, ctx, resizeN);
+  const topRightCorner = createButtonWithOffsets(eleId, 2, 2);
   addHandlersToCornerButton(topRightCorner, eleId, ctx, resizeNE);
-  const bottomRightCorner = cornerButton(eleId, false, true, true);
+  const rightButton = createButtonWithOffsets(eleId, 0, 2, 1);
+  addHandlersToCornerButton(rightButton, eleId, ctx, resizeE);
+  const bottomRightCorner = createButtonWithOffsets(eleId, 0, 2, 2);
   addHandlersToCornerButton(bottomRightCorner, eleId, ctx, resizeSE);
-  const bottomLeftCorner = cornerButton(eleId, false, false, true, true);
+  const bottomButton = createButtonWithOffsets(eleId, 0, 1, 2);
+  addHandlersToCornerButton(bottomButton, eleId, ctx, resizeS);
+  const bottomLeftCorner = createButtonWithOffsets(eleId, 0, 0, 2, 2);
   addHandlersToCornerButton(bottomLeftCorner, eleId, ctx, resizeSW);
+  const leftButton = createButtonWithOffsets(eleId, 0, 0, 1, 2);
+  addHandlersToCornerButton(leftButton, eleId, ctx, resizeW);
 
   div.appendChild(topLeftCorner);
+  div.appendChild(topButton);
   div.appendChild(topRightCorner);
+  div.appendChild(rightButton);
   div.appendChild(bottomRightCorner);
+  div.appendChild(bottomButton);
   div.appendChild(bottomLeftCorner);
+  div.appendChild(leftButton);
 
   // rotate button
-  const rotateButton = cornerButton(eleId);
+  const rotateButton = createButtonWithOffsets(eleId);
   rotateButton.id = `${ROTATE_BUTTON_PRE}-${eleId}`;
   setStyles(rotateButton, ROTATE_BUTTON_STYLES);
   addHandlersToRotateButton(rotateButton, eleId, ctx);
@@ -133,30 +161,67 @@ function addToolsToSelectionDiv(
 
 const CORNER_BUTTON_OFFSET = -12;
 
-function cornerButton(
+// function createButtonWithOffsets(
+//   eleId: string,
+//   top?: boolean,
+//   right?: boolean,
+//   bottom?: boolean,
+//   left?: boolean
+// ): HTMLButtonElement {
+//   const button = document.createElement("button");
+//   setStyles(button, SELECTED_CORNER_BUTTON);
+//   let buttonId = "";
+//   if (top) {
+//     button.style.top = `${CORNER_BUTTON_OFFSET}px`;
+//     buttonId += "n";
+//   } else if (bottom) {
+//     button.style.bottom = `${CORNER_BUTTON_OFFSET}px`;
+//     buttonId += "s";
+//   }
+//   if (right) {
+//     button.style.right = `${CORNER_BUTTON_OFFSET}px`;
+//     buttonId += "e";
+//   } else if (left) {
+//     button.style.left = `${CORNER_BUTTON_OFFSET}px`;
+//     buttonId += "w";
+//   }
+//   button.id = `${CORNER_BUTTON_PRE}-${buttonId}-${eleId}`;
+//   button.style.cursor = `${buttonId}-resize`;
+//   return button;
+// }
+
+// each can be in a range of [0, 3) (3 exclusive)
+function createButtonWithOffsets(
   eleId: string,
-  top?: boolean,
-  right?: boolean,
-  bottom?: boolean,
-  left?: boolean
+  top: IntRange<0, 3> = 0,
+  right: IntRange<0, 3> = 0,
+  bottom: IntRange<0, 3> = 0,
+  left: IntRange<0, 3> = 0
 ): HTMLButtonElement {
   const button = document.createElement("button");
   setStyles(button, SELECTED_CORNER_BUTTON);
   let buttonId = "";
-  if (top) {
+  if (top === 2) {
     button.style.top = `${CORNER_BUTTON_OFFSET}px`;
     buttonId += "n";
-  } else if (bottom) {
+  }
+  if (bottom === 2) {
     button.style.bottom = `${CORNER_BUTTON_OFFSET}px`;
     buttonId += "s";
+  } else if (bottom === 1) {
+    button.style.bottom = `calc(50% - ${SELECTED_BUTTON_HEIGHT / 2}px)`;
   }
-  if (right) {
+  if (right === 2) {
     button.style.right = `${CORNER_BUTTON_OFFSET}px`;
     buttonId += "e";
-  } else if (left) {
+  } else if (right === 1) {
+    button.style.right = `calc(50% - ${SELECTED_BUTTON_WIDTH / 2}px)`;
+  }
+  if (left === 2) {
     button.style.left = `${CORNER_BUTTON_OFFSET}px`;
     buttonId += "w";
   }
+
   button.id = `${CORNER_BUTTON_PRE}-${buttonId}-${eleId}`;
   button.style.cursor = `${buttonId}-resize`;
   return button;
